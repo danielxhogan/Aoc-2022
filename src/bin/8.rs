277a5602c8662d569b55d9261d67b1cac19edfb2
danielxhogan <danielxhogan@gmail.com>
2023-08-17 17:07:42 -0700

    day 8 solution

--- src/bin/8.rs
+++ src/bin/8.rs
@@ -1,43 +1,5 @@
 use std::collections::HashMap;
-use std::{cell::RefCell, iter::Enumerate, rc::Rc, str::Lines};
-
-struct Tree {
-    height: usize,
-    col: usize,
-    row: usize,
-    distance_left: usize,
-    distance_up: usize,
-    sight_left: Option<usize>,
-    sight_right: Option<usize>,
-    sight_up: Option<usize>,
-    sight_down: Option<usize>,
-    sight_total: usize,
-}
-
-impl Tree {
-    fn new(height: usize, col: usize, row: usize) -> Rc<RefCell<Tree>> {
-        Rc::new(RefCell::new(Tree {
-            height,
-            col,
-            row,
-            distance_left: 0,
-            distance_up: 0,
-            sight_left: None,
-            sight_right: None,
-            sight_up: None,
-            sight_down: None,
-            sight_total: 0,
-        }))
-    }
-
-    fn incr_dist_left(&mut self) {
-        self.distance_left += 1;
-    }
-
-    fn incr_dist_up(&mut self) {
-        self.distance_up += 1;
-    }
-}
+use std::{iter::Enumerate, str::Lines};
 
 fn make_visible_key(col: usize, row: usize) -> String {
     format!("{}, {}", col, row)
@@ -126,7 +88,6 @@ fn calculate_visible(mut lines: Enumerate<Lines>) {
 // part 2
 fn calculate_scenic_score(mut lines: Enumerate<Lines>) {
     let mut forest: Vec<Vec<(usize, usize)>> = Vec::new();
-    let mut col;
     let mut row;
 
     while let Some(line) = lines.next() {
@@ -135,186 +96,87 @@ fn calculate_scenic_score(mut lines: Enumerate<Lines>) {
         let mut trees = line.1.chars().enumerate();
 
         while let Some(tree) = trees.next() {
-            col = tree.0;
             let current_height: usize = tree.1.to_string().parse().unwrap();
             forest[row].push((current_height, 0));
         }
     }
 
+    let mut scene_score;
+    let mut highest_score = 0;
     let mut delta;
+    let mut trees;
 
     for i in 0..forest.len() {
-        println!("{}", forest[i][forest[0].len() - 1].0);
         for j in 0..forest[i].len() {
+            scene_score = 1;
+
+            // looking left
             delta = j;
+            trees = 0;
 
             while delta > 0 {
                 delta -= 1;
-                forest[i][j].1 += 1;
+                trees += 1;
 
                 if forest[i][delta].0 >= forest[i][j].0 {
                     break;
                 }
             }
 
+            scene_score *= trees;
+
+            // looking right
             delta = j;
+            trees = 0;
 
             while delta < forest[i].len() - 1 {
                 delta += 1;
-                forest[i][j].1 += 1;
+                trees += 1;
 
                 if forest[i][delta].0 >= forest[i][j].0 {
                     break;
                 }
             }
 
+            scene_score *= trees;
+
+            // looking up
             delta = i;
+            trees = 0;
 
             while delta > 0 {
                 delta -= 1;
-                forest[i][j].1 += 1;
+                trees += 1;
 
                 if forest[delta][j].0 >= forest[i][j].0 {
                     break;
                 }
             }
 
+            scene_score *= trees;
+
+            // looking down
             delta = i;
+            trees = 0;
 
             while delta < forest.len() - 1 {
                 delta += 1;
-                forest[i][j].1 += 1;
+                trees += 1;
 
                 if forest[delta][j].0 >= forest[i][j].0 {
                     break;
                 }
             }
-        }
-    }
 
-    let mut highest_scene_score = 0;
+            scene_score *= trees;
 
-    for i in 0..forest.len() {
-        for j in 0..forest[i].len() {
-            if forest[i][j].1 > highest_scene_score {
-                highest_scene_score = forest[i][j].1;
+            if scene_score > highest_score {
+                highest_score = scene_score;
             }
         }
     }
 
-    println!("highest scene score: {}", highest_scene_score);
-
-
-
-
-    // let mut all_trees: Vec<Rc<RefCell<Tree>>> = Vec::new();
-    // let mut seen_row: Vec<Rc<RefCell<Tree>>> = Vec::new();
-    // let mut seen_col: HashMap<usize, Vec<Rc<RefCell<Tree>>>> = HashMap::new();
-
-    // let mut row = 0;
-    // let mut col = 0;
-
-    // while let Some(line) = lines.next() {
-    //     row = line.0;
-    //     let mut trees = line.1.chars().enumerate();
-
-    //     while let Some(tree) = trees.next() {
-    //         col = tree.0;
-    //         let current_height: usize = tree.1.to_string().parse().unwrap();
-    //         let new_tree = Tree::new(current_height, col, row);
-    //         let mut new_tree_borrow = new_tree.borrow_mut();
-
-    //         let mut i = seen_row.len();
-
-    //         while i > 0 {
-    //             i -= 1;
-    //             let mut current_tree = seen_row[i].borrow_mut();
-    //             current_tree.incr_dist_left();
-
-    //             if current_tree.height >= current_height && new_tree_borrow.sight_left == None {
-    //                 new_tree_borrow.sight_left = Some(current_tree.distance_left);
-    //                 new_tree_borrow.sight_total += current_tree.distance_left;
-    //             }
-
-    //             if current_height >= current_tree.height && current_tree.sight_right == None {
-    //                 current_tree.sight_right = Some(current_tree.distance_left);
-    //                 current_tree.sight_total += current_tree.distance_left;
-    //             }
-    //         }
-
-    //         if new_tree_borrow.sight_left == None {
-    //             new_tree_borrow.sight_left = Some(col);
-    //             new_tree_borrow.sight_total = col;
-    //         }
-
-    //         seen_row.push(new_tree.clone());
-
-    //         let current_col_maybe = seen_col.get_mut(&col);
-
-    //         match current_col_maybe {
-    //             None => {
-    //                 new_tree_borrow.sight_up = Some(0);
-    //                 seen_col.insert(col, vec![new_tree.clone()]);
-    //             }
-    //             Some(current_col) => {
-    //                 let mut i = current_col.len();
-
-    //                 while i > 0 {
-    //                     i -= 1;
-    //                     let mut current_tree = current_col[i].borrow_mut();
-    //                     current_tree.incr_dist_up();
-
-    //                     if current_tree.height >= current_height && new_tree_borrow.sight_up == None {
-    //                         new_tree_borrow.sight_up = Some(current_tree.distance_up);
-    //                         new_tree_borrow.sight_total += current_tree.distance_up;
-    //                     }
-
-    //                     if current_height >= current_tree.height && current_tree.sight_down == None {
-    //                         current_tree.sight_down = Some(current_tree.distance_up);
-    //                         current_tree.sight_total += current_tree.distance_up;
-    //                     }
-    //                 }
-
-    //                 if new_tree_borrow.sight_up == None {
-    //                     new_tree_borrow.sight_up = Some(row);
-    //                     new_tree_borrow.sight_total = row;
-    //                 }
-
-    //                 current_col.push(new_tree.clone());
-    //             }
-    //         }
-    //     }
-
-    //     for i in 0..seen_row.len() {
-    //         let mut current_tree = seen_row[i].borrow_mut();
-    //         if current_tree.sight_right == None {
-    //             current_tree.sight_right = Some(col - current_tree.col);
-    //             current_tree.sight_total = col - current_tree.col;
-    //         }
-    //     }
-
-    //     for _ in 0..seen_row.len() {
-    //         all_trees.push(seen_row.pop().unwrap());
-    //     }
-    // }
-
-    // let mut highest_scene_score = 0;
-
-    // for trees in seen_col.values() {
-    //     for tree in trees {
-    //         let mut tree_borrow = tree.borrow_mut();
-    //         if tree_borrow.sight_down == None {
-    //             tree_borrow.sight_down = Some(row - tree_borrow.row);
-    //             tree_borrow.sight_total += row - tree_borrow.row;
-    //         }
-
-    //         if tree_borrow.sight_total > highest_scene_score {
-    //             highest_scene_score = tree_borrow.sight_total;
-    //         }
-    //     }
-    // }
-
-    // println!("highest scene score: {}", highest_scene_score);
+    println!("highest score: {}", highest_score);
 }
 
 fn main() {
